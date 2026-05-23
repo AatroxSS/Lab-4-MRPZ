1de8f281489874397a35900eecea04bf28ba672e
AatroxSS <arsenijruslanovic@gmail.com>
2026-05-23 16:01:56 +0300

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args)
+    {
+        System.out.println("Привіт, Світ!");
+        int target = 123;
+        String message = "Перевірка файлового вводу/виводу в Java";
+        System.out.println(message + " " + target);
+    }
+}
